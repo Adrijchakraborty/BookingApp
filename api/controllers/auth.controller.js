ce8a3ae0a4e8a9e128ab95ae0e04241ca0c8b242
Adrijchakraborty <adrijchakraborty8@gmail.com>
2024-06-25 16:07:00 +0530

    auth login done

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,16 +1,39 @@
 import User from "../models/User.js";
+import bcrypt from "bcryptjs"
+import { createError } from "../utils/error.js";
 
 export const registerUser = async(req, res, next) =>{
     try {
+        var salt = bcrypt.genSaltSync(10);
+        var hash = bcrypt.hashSync("B4c0/\/", salt);
+
         const newUser = await User({
             username: req.body.username,
             email: req.body.email,
-            password: req.body.password
+            password: hash
         })
 
         await newUser.save();
 
         res.status(200).json("User created successfully");
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const login = async(req, res, next) =>{
+    try {
+        const user = await User.findOne({username: req.body.username});
+
+        if(!user) return next(createError(404, "User not found"));
+
+        const ispass = bcrypt.compare(req.body.password, user.password);
+
+        if(!ispass) return next(createError(400,"wrong credentials"));
+
+        const {password,admin,...other} = user._doc;
+
+        res.status(200).json({...other});
     } catch (error) {
         next(error);
     }
